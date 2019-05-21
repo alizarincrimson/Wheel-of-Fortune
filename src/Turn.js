18abb200074705a4929954893d0de20042b967df
Ann Cerveny <cervann11@gmail.com>
2019-05-21 09:56:37 -0600

    Add pseudocode to Turn and Wheel class

--- src/Turn.js
+++ src/Turn.js
@@ -1,16 +1,41 @@
 class Turn {
-  constructor() {
-
+  constructor(guess) {
+    this.guess = guess;
   }
-  buyVowel() {
-
-  }
-
-  spinWheel() {
-
+  buyVowel(player) {
+    // player.score = player.score - 100
+    // prompt to select available vowel from vowels array
+    // chosen vowel spliced from array
   }
 
-  solvePuzzle() {
-    
+  spinWheel(player) {
+    // wheel.getWheelValue()
+    // if (wheel.getWheelValue().spin === 'Bankrupt') {
+    // player.score === 0
+    // return `Your score has been reset to 0`
+    // } else if (wheel.getWheelValue().spin === 'Lose A Turn'){
+    // switch to next player
+    // return `You have lost a turn.`
+    // } else {
+    // player selects consonant from consonants array
+    // consonant is spliced from consonants array
+    // solvePuzzle(guess)
+    // }
+    //
+    //
+  }
+
+  solvePuzzle(player, guess) {
+    // if (guess === puzzle.word.letter) {
+    //  player.score = player.score + wheel.getWheelValue().spin  
+    //  return `Correct! ${player} receives ${wheel.getWheelValue().spin}`
+    // } else if (guess != puzzle.word.letter) {
+    //  player.score = player.score - wheel.getWheelValue().spin
+    //  return `You've guessed incorrectly. Your score is now ${player.score}.`
+    // } else if (guess === puzzle.word) {
+    //  player.score = player.score + wheel.getWheelValue().spin 
+    //  return `Congratulations! You've solved the puzzle!`
+    // }
+    //}
   }
 }
--- src/Wheel.js
+++ src/Wheel.js
@@ -3,6 +3,10 @@ class Wheel {
     this.dataFilePath = dataFilePath;
   }
   getWheelValue() {
-
+    //use Math.random to select Wheel value
+    // let wheel = [200, 300, 500, 600, 900, 'Bankrupt', 'Lose A Turn']
+    // let spin = function getRandomInt(wheel) {
+    // return Math.floor(Math.random() * Math.floor(wheel));
+}
   }
 }
