14b9f392c30ce60ebfb4dd08b75244375eb619e1
Hannah Hyde <hannahhyde@gmail.com>
2019-05-20 20:16:20 -0600

    Pseudocode for Round, Puzzle, PuzzleSet

---
+++ src/PuzzleSet.js
@@ -0,0 +1,22 @@
+import data from Data;
+
+class PuzzleSet {
+  constructor() {
+    this.puzzles = [];
+  }
+
+  getPuzzleSet() {
+    // get into data filter, obj puzzles, each obj of puzzles, key of puzzle_bank
+    // randomly grab 5
+    // return array of objects
+    
+    // data drill maybe? - data.puzzles[numAnswers].filter
+    // 
+
+    // does this make sense to have it's own class or could Puzzle do it?
+    //  
+  }
+
+}
+
+export default PuzzleSet;
--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 
 export default {
+  
 
 }
