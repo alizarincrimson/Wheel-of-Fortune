73e3433a335330768e782cbfb12eced4f7bb3be0
AntonioFry <43049505+AntonioFry@users.noreply.github.com>
2019-05-20 20:59:20 -0600

    Revert "Pseudocode for Round, Puzzle, PuzzleSet"

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,27 +1,8 @@
-import domUpdates from './domUpdates';
-import PuzzleSet from './PuzzleSet';
-
 class Puzzle {
-  constructor() {
-    this.puzzle = this.PuzzleSet.pop();
-    this.category = null || puzzle.category;
-    this.numLetters = puzzle.total_number_of_letters
-    this.numWords = null || puzzle.number_of_words;
-    this.correctAnswer = null || puzzle.correct_answer.toUpperCase();
-    this.splitAnswer = this.correctAnswer.toUpperCase().split('');
+  constructor(dataFilePath) {
+    this.dataFilePath = dataFilePath;
   }
+  getPuzzle() {
 
-  setupRound() {
-    // show this.puzzle.category on the DOM
-    // count number of words, count letters
-    // use this.splitAnswer to populate spots on board based on number of words and length of each word, avoid weird breaks and push entire word gets down to the next line when out of space
   }
-
-  solvePuzzle(guess) {
-    // if the current player's turn input.toUpperCase() === this.correctAnswer
-    // fire method on Turn to pass winner back to Round?
-  }
-
 }
-
-export default Puzzle;
--- src/Round.js
+++ src/Round.js
@@ -1,47 +1,11 @@
-import Puzzle from './Puzzle';
-import Wheel from './Wheel';
-import Turn from './Turn';
-
 class Round {
-  constructor(puzzle, players, wheel) {
-    this.puzzle = new Puzzle();
-    this.players = players;
-    this.turn = new Turn(this.startTurn());
-    this.currentPlayer = null;
-    this.wheel = Wheel;
-    this.p1Score = 0;
-    this.p2Score = 0;
-    this.p3Score = 0;
+  constructor(puzzle) {
+    this.puzzle = puzzle;
   }
-
-  startTurn(currentPlayer) {
-    // passes the new turn a player
-    // fires on start of new round and when a player's turn ends
-
-    // switch statement? 
-    // case: currentPlayer === null, get a random player
-    // case: if current player is players index 2 (the last player)
-    // then assign currentPlayer to index 0
-    // case: assign currentPlayer to players[i] + 1
-  }
-  
-  updateRoundScore(spinValue) {
-  // check current player
-  // if this.players[this.currentPlayer]
-  // if (value of turn) exists
-  // local round score += spinValue
-  // I actually think this might live in Turn?
-  }
-
-  roundWinner(player) {
-    // switch statement?
-    // case: player is players[0]
-    // pass this.p1Score to method for adding points players[0].score
-    // case: player is players[1]
-    // pass this.p2Score to method for players[1]
-    // case: player is players[2]
-    // pass this.p3Score to method for players[3]
-    // tell Game to make a new Round, do we want a new wheel?
+  roundWinner() {
+  // whoever solves puzzle gets round score.
+  // take palayer round score and at it to their total score.
+  // clear roundscore for all players. 
   }
 
   roundEnd() {
