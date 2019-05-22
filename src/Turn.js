90456827566e335420556d9466908fc18ca939b4
Antonio Fry <antoniofry2000@gmail.com>
2019-05-22 17:32:22 -0600

    add pseudo code for methods

--- src/Turn.js
+++ src/Turn.js
@@ -1,6 +1,8 @@
 class Turn {
-  constructor(players, wheel) {
+  constructor(players, wheel, puzzle) {
     this.players = players;
+    this.wheel = wheel;
+    this.puzzle = puzzle
     this.currentPlayer = players[0];
   }
   
@@ -13,19 +15,37 @@ class Turn {
   }
 
   buyVowel(vowel) {
-    // when you buy a vowel
+    // the vowel is going to passed though this method as an argument when it is clicked on.
+    // then the vowel will be passed through as an argument for the evaluateGuess method. 
   }
 
   spinWheel() {
-
+    // this will invoke the wheels method of getRandomValue.
+    // it will then wait for user to emter in a letter as a guess
+    // that guess should be uppercased
+    // that guess should be passed through evaluate guess as an argument
   }
 
   solvePuzzle() {
-    
+    // the user will enter in the whole puzzle WITH SPACES
+    // it will then need to evaluate the guess of the player
+    // if guess stricly equals the joined puzzle(in array form)
+      // return true
+    //  otherwise 
+      // invoke getCurrentPlayer
+      // and return false
   }
 
-  evaluateGuess(guess) {
-
+  evaluateLetterGuess(guess) {
+    // takes in the guess from either spinWheel or buyVowel method
+    // it evaluates if the the puzzle includes the guess value
+    // if the puzzle does include the guess value
+      // splice the value(s) of guess from the puzzle where it's included
+      // and return true
+    // otherwise
+      // invoke getCurrentPlayer
+      // and return false
+    // 
   }
 }
 
