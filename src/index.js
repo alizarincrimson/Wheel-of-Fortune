54b7c6903f5bcd29af3e437fc85ec92ececa5c5f
Ann Cerveny <cervann11@gmail.com>
2019-05-22 16:11:06 -0600

    Fetch dataset of questions
    Co-authored-by: Hannah Hyde <HannahHyde@gmail.com>
    Co-authored-by: Ann Cerveny <CervAnn11@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@
 import $ from 'jquery';
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/base.scss';
+// import './Scss/base';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
-import Game from './js/game';
-import domUpdates from './js/domUpdates';
+import Game from './Game';
+import domUpdates from './domUpdates';
 
 console.log('This is the JavaScript entry file - your code begins here.');
 
